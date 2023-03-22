adf6c77756d251555eb07f1ee0e3010c23177057
João Lucas Carvalho <jlprcarvalho@gmail.com>
2023-03-22 18:03:27 -0300

    method speake ready

--- src/speaker/Terminal.py
+++ src/speaker/Terminal.py
@@ -2,18 +2,24 @@ import sys
 from pathlib import Path
 import subprocess
 class Terminal:
+
+
+# Open a new command prompt window in the C:\Windows\System32 directory
+#subprocess.run(['cmd', '/c', 'start', '/D', 'C:\\Windows\\System32'])
+
     def __init__(self):
         #self.test()
-        self.folder = Path(r"C:Users\Joao\Downloads\balcon")
+        self.folder = Path(r"C:\Users\Joao\Downloads\balcon")
+
         self.speake("Hello World")
-    def test(self):
-        try:
-            subprocess.check_output("dir {}", shell=True, stderr=subprocess.STDOUT)(format = self.folder)
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+
+
     def speake(self, text):
-       cmd = {r"C:Users\Joao\Downloads\balcon.exe"}
-       cmd.onecmd(cmd, "-l", "oi", shell=True )
+       subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', '-t', text])
+
+    def change_voice(self, voice):
+        subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', ' --voice1-name', voice])
 
 if __name__ == "__main__":
     Terminal()
