a4ff03e0ea3557b3f54ce8183ba640f87cc24883
João Lucas Carvalho <jlprcarvalho@gmail.com>
2023-03-28 17:43:04 -0300

    Developing the  predictor

--- src/speaker/Editor.py
+++ src/speaker/Editor.py
@@ -1,6 +1,15 @@
-class Editor():
-    def __init__(self):
-        pass
-    def write_char(self, char):
-        print(char)
-
+import TextBlob
+#class Editor():
+    # def __init__(self):
+    #     pass
+    # def predict(self, text, words=9):
+    #     blob = TextBlob(text)
+    #     return blob.ngrams(words)
+    # def get_words(self, text):
+    #     blob = TextBlob(text)
+    #     return blob.words
+    #
+    #
+blob = TextBlob("This is a test")
+test = blob.ngrams(5)
+print(test)
