c6c4a8ff2c25ab288cf22dff0d3a0b3f115a038b
João Lucas Carvalho <jlprcarvalho@gmail.com>
2023-05-19 16:56:14 -0300

    Adding menu

--- src/speaker/Interface.py
+++ src/speaker/Interface.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Text, Button, Frame, Canvas, OptionMenu
+from tkinter import Tk, Text, Button, Frame, Canvas, OptionMenu, StringVar, ttk
+
 from Keyboard import Keyboard as kb
 
 
@@ -12,22 +13,29 @@ class Interface:
     #  To Solve
     buttonSpeake = Button(root, text="Speak", height=20, width=10).place(x=780, y=30)
     frame_favorite = Frame(root, width=300, height=300, background="yellow", border=2).place(x=0, y=30)
-    frame_keyboard = Frame(root, width=1024, height=600, background="#07C7F2").place(x=0, y=300)
+    frame_keyboard = Frame(root, width=1024, height=600, background="#07C7F2").place(x=60, y=300)
     keyboard = kb(frame_keyboard, text_field)
     toolbar = Canvas(root, width=1024, height=30, background="black", border=2)
     toolbar.place(x=0, y=0)
 
-    button_favorite = toolbar.create_oval(100, 90, 5, 70, fill="#07C7F2", tags="button_favorite")
-    button_place = toolbar.move("button_favorite", 275, -65)
-    text_favorite = toolbar.create_text(320, 15, text="Favoritar", fill="white", font=11, tags="text_favorite")
+    button_favorite = toolbar.create_oval(100, 90, 5, 67, fill="#07C7F2", tags="button_favorite")
+    button_place = toolbar.move("button_favorite", 275, -62)
+    text_favorite = toolbar.create_text(325, 15, text="Favoritar", fill="white", font=11, tags="text_favorite")
     #  toolbar.tag_bind("button_favorite", "<Button-1>", ffs.Functions_favorite_section(frame_favorite, r"D:\Documentos\teste.txt").add_favorite(Functions_keyboard.Functions_keyboard().get_text(text_field)))
-    voices = ["test"]
-    voice = "test"
-    voice_choose = OptionMenu(root, "Selecione uma voz", voice, *voices)
-    voice_choose.place(x=500, y=0)
-    voice_choose.config(width=20, height=1, background="white", border=2)
+    voices = ["test", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9"]
+    voice = StringVar()
+    voice.set("Selecione uma voz")
+    voice_choose = ttk.Combobox(frame_favorite, textvariable=voice, values=voices)
+    voice_choose.place(x=500, y=8)
 
+    voice_choose.config(width=20, height=1, background="white")
+    menu_button = Button(frame_keyboard, text="☰").place(x=1000, y=8)
 
     root.mainloop()
+
     def get_text_field(self):
         return self.text_field
+
+    def get_voice(self):
+        voice = self.voice.get()
+        return voice
