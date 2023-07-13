97a76968f438bbdf3e5d3323c3e8668d47640efa
Joaolpridolficarvalho <jlprcarvalho@gmail.com>
2023-07-13 14:48:56 -0300

    Revert "Speak ok"
    This reverts commit fab99c2e80d12ea17ecd59d35e72e47bef6b10de.

--- src/speaker/Terminal.py
+++ src/speaker/Terminal.py
@@ -1,18 +1,15 @@
 import sys
+from pathlib import Path
 import subprocess
-from Functions_keyboard import Functions_keyboard as fk
 
 
 class Terminal:
 
-    def __init__(self, text_field):
-        self.text_field = text_field
-        self.fk = fk(text_field=self.text_field)
+    def __init__(self):
+        self.folder = Path(r"D:\Downloads\balcon")
 
-    def speak(self):
-    	text = self.fk.get_text()
-    	command = ['nohup espeak-ng '+ text]
-    	subprocess.Popen(command, shell=True)
+    def speak(self, text):
+        subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', '-t', text])
 
     def change_voice(self, voice):
         subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', ' --voice1-name', voice])
