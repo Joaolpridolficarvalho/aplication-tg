fab99c2e80d12ea17ecd59d35e72e47bef6b10de
Joaolpridolficarvalho <jlprcarvalho@gmail.com>
2023-07-13 10:31:22 -0300

    Speak ok

--- src/speaker/Interface.py
+++ src/speaker/Interface.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, Button, Frame, StringVar, ttk
 from Instance_controller import Instance_controller as ic
 from Textboxes import Textboxes as tb
-
+from Terminal import Terminal
 class Interface:
     root = Tk()
     root.geometry("1024x600")
@@ -11,11 +11,12 @@ class Interface:
 
 
     #  To Solve
-    buttonSpeake = Button(root, text="Speak", height=20, width=10).place(x=780, y=30)
+    text_field = tb(root).text_field()
+    buttonSpeake = Button(root, text="Speak", height=20, width=10, command=Terminal(text_field).speak).place(x=780, y=30)
     frame_favorite = Frame(root, width=300, height=300, background="yellow", border=2).place(x=0, y=30)
     frame_keyboard = Frame(root, width=1024, height=600, background="#07C7F2").place(x=0, y=300)
 
-    ic(root,frame_favorite, frame_keyboard, tb(root).text_field())
+    ic(root,frame_favorite, frame_keyboard, text_field)
     voices = ["test", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9"]
     voice = StringVar()
 
--- src/speaker/Terminal.py
+++ src/speaker/Terminal.py
@@ -1,15 +1,18 @@
 import sys
-from pathlib import Path
 import subprocess
+from Functions_keyboard import Functions_keyboard as fk
 
 
 class Terminal:
 
-    def __init__(self):
-        self.folder = Path(r"D:\Downloads\balcon")
+    def __init__(self, text_field):
+        self.text_field = text_field
+        self.fk = fk(text_field=self.text_field)
 
-    def speak(self, text):
-        subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', '-t', text])
+    def speak(self):
+    	text = self.fk.get_text()
+    	command = ['nohup espeak-ng '+ text]
+    	subprocess.Popen(command, shell=True)
 
     def change_voice(self, voice):
         subprocess.run(['cmd', '/c', 'start', '/D', self.folder, 'balcon.exe', ' --voice1-name', voice])
